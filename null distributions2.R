1c8cef6820dc1d3f8e3c074b069f6d9aa71726d3
Rimao <74907615+Rimao-Zugman@users.noreply.github.com>
2023-01-16 14:33:52 +0100

    Delete null distributions2.R

--- null distributions2.R
+++
@@ -1,6 +0,0 @@
-library(downloader) 
-url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
-filename <- basename(url)
-download(url, destfile=filename)
-x <- unlist( read.csv(filename) )
-set.seed(1)
